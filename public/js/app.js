c55e06abfca039cf01289787ee7369b98987272f
Hamdi-Limam <limamhamdi13@gmail.com>
2019-11-22 15:09:02 +0100

    My Project

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+const weatherform=document.querySelector('form')
+const search =document.querySelector('input')
+const firstmessage=document.querySelector('#message1')
+const secondmessage=document.querySelector('#message2')
+weatherform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location= search.value
+    
+    fetch("http://localhost:3000/weather?address="+location).then((response)=>{
+        
+    response.json().then((data={})=>{
+        if (data.error){firstmessage.textContent=data.error
+            secondmessage.textContent=''
+}
+        else{
+            firstmessage.textContent=data.location
+            secondmessage.textContent=data.forecast
+        }
+    })
+})
+})
---
+++ src/app.js
@@ -0,0 +1,84 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const path= require('path')
+const express= require('express')
+const hbs=require('hbs')
+
+const app=express()
+
+//define paths for express config
+const dir=path.join(__dirname,'../public')
+const viewpath=path.join(__dirname,'../templates/views')
+const partialpath=path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewpath)
+hbs.registerPartials(partialpath)
+
+//setup static directory to serve
+app.use(express.static(dir))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Hamdi Nait Limam'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Hamdi Nait Limam'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'Hamdi Nait Limam'
+    })
+})
+
+app.get('/weather', (req, res) => {
+   if(!req.query.address){
+    return res.send({
+        error:'Please provide an address'
+    })
+   }
+   geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+     if (error){
+         return res.send({error})
+     }
+     forecast(latitude,longitude,(error,forecastData)=>{
+         if(error){
+            return  res.send({error})
+         }
+         res.send({
+             forecast:forecastData,
+             location,
+             address:req.query.address
+         })
+     })
+   })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('error',{
+        title:'404',
+        name:'Hamdi Nait Limam',
+        Errormsg:"Help Page Not Found"
+    })
+})
+
+
+app.get('*',(req,res)=>{
+   res.render('error',{
+    title:'404',
+    name:'Hamdi Nait Limam',
+    Errormsg:"Page Not Found"
+   })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on Port 3000')
+})
